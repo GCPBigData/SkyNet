df7827101d8b91baa3977367751158e44a49e97c
Jose R F Junior <web2ajax@gmail.com>
2020-11-06 12:30:38 -0300

    first commit

---
+++ src/app/tecnico/create-tecnico/create-tecnico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-tecnico',
+  templateUrl: './create-tecnico.component.html',
+  styleUrls: ['./create-tecnico.component.css']
+})
+export class CreateTecnicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
